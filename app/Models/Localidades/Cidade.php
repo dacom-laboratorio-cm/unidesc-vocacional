09acbaca22ab0b07b1b2b699246f40b630928b50
virocha11 <vivianadrianarocha@gmail.com>
2024-08-15 21:53:33 -0300

    alterações no candidato e cidade

--- app/Models/Localidades/Cidade.php
+++ app/Models/Localidades/Cidade.php
@@ -5,27 +5,27 @@ namespace App\Models\Localidades;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class Cidade extends Model
+class Escola extends Model
 {
-    protected $fillable = ['nm_cidade', 'estado_id'];
+    protected $fillable = ['nm_escola'];
 
-    public function estado()
-    {
-        return $this->hasOne(Estado::class, 'id', 'estado_id');
-    }
+    // public function estado()
+    // {
+    //     return $this->hasOne(Estado::class, 'id');
+    // }
 
-    public static function cidadesByEstado($sigla)
+    public static function getEscolas()
     {
-        return DB::table('cidades')
-            ->join('estados', 'estado_id', '=', 'estados.id')
-            ->orWhere('sg_estado', $sigla)
-            ->orWhere('cidades.estado_id', $sigla)
+        return DB::table('escolas')
+            // ->join('escolas', 'id', '=', 'id')
+            // ->orWhere('sg_estado', $sigla)
+            // ->orWhere('cidades.estado_id', $sigla)
             ->select
             (
-                'cidades.id AS cidade_id',
-                'nm_cidade',
-                'nm_estado',
-                'sg_estado'
+                // 'cidades.id AS cidade_id',
+                'nm_escola'
+                // 'nm_estado',
+                // 'sg_estado'
             )
             ->get();
     }
